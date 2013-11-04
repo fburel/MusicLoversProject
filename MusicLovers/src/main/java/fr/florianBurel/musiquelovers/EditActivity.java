e7189041fb7e7b495fde0c6a6becaae99c80fb7e
Florian Burel <fl0@mac.com>
2013-11-04 16:21:18 +0100

    Ajout d'une property music a l'activity Ajout des id dans le layout preparation des variable

--- MusicLovers/src/main/java/fr/florianBurel/musiquelovers/EditActivity.java
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/EditActivity.java
@@ -2,13 +2,31 @@ package fr.florianBurel.musiquelovers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 /**
  * Created by fl0 on 04/11/2013.
  */
-public class EditActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+public class EditActivity extends Activity
+{
+    Music music;
+
+    private EditText musicTitleEditText;
+    private RadioButton musicLikeRadioBtn;
+    private RadioButton musicDontLikeRadioBtn;
+    private EditText musicArtistEditText;
+    private EditText musicCategoryEditText;
+    private EditText musicDescriptionEditText;
+
+
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_layout);
+
+        this.music = new Music();
+
+
     }
 }
