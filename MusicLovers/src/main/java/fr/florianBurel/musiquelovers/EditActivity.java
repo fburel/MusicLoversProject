d8785e8fe3f7eb2a4c2178f4089a26d23fc66722
Florian Burel <fl0@mac.com>
2013-11-04 15:18:32 +0100

    Ajout d'une nouvelle activity EditActivity

---
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/EditActivity.java
@@ -0,0 +1,14 @@
+package fr.florianBurel.musiquelovers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by fl0 on 04/11/2013.
+ */
+public class EditActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_layout);
+    }
+}
