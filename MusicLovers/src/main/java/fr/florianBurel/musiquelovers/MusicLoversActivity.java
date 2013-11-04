66302b2bb489b5e0bdd7d2089e7b80cf5531420b
Florian Burel <fl0@mac.com>
2013-11-05 00:03:56 +0100

    Recuperation de la liste des music via la méthode static Affichage de la liste avec une ArrayAdapter

--- MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
@@ -2,7 +2,11 @@ package fr.florianBurel.musiquelovers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by fl0 on 04/11/2013.
@@ -11,14 +15,22 @@ public class MusicLoversActivity extends Activity {
 
     private ListView listView;
 
+    private ArrayList<Music> musics;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
         bind();
+
+        this.musics = Music.getAllMusics();
+
+        this.listView.setAdapter(new ArrayAdapter<Music>(this, android.R.layout.simple_list_item_1, this.musics));
+
     }
 
-    private void bind() {
+    private void bind()
+    {
         this.listView = (ListView) findViewById(R.id.listView);
     }
 }
