7984086cb32f66f8c84b79ad0dacc29d3032da8f
Florian Burel <fl0@mac.com>
2013-11-04 23:44:16 +0100

    Ajout d'une listView dans le layout + bind a l'activity

--- MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
@@ -2,13 +2,23 @@ package fr.florianBurel.musiquelovers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 /**
  * Created by fl0 on 04/11/2013.
  */
 public class MusicLoversActivity extends Activity {
+
+    private ListView listView;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        bind();
+    }
+
+    private void bind() {
+        this.listView = (ListView) findViewById(R.id.listView);
     }
 }
