88b7456d89994e26a7f6890fc8f18cf952a99106
Florian Burel <fl0@mac.com>
2013-11-04 16:45:36 +0100

    Implementation de la méthode bind() et refresh()

--- MusicLovers/src/main/java/fr/florianBurel/musiquelovers/EditActivity.java
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/EditActivity.java
@@ -27,6 +27,30 @@ public class EditActivity extends Activity
 
         this.music = new Music();
 
+        bind();
+
+        refresh();
+
+
+    }
+
+    private void refresh() {
+        this.musicTitleEditText.setText(this.music.getName());
+        this.musicArtistEditText.setText(this.music.getAuthor());
+        this.musicCategoryEditText.setText(this.music.getCategory());
+        this.musicDescriptionEditText.setText(this.music.getDescription());
+        this.musicLikeRadioBtn.setChecked(this.music.isLiked());
+        this.musicDontLikeRadioBtn.setChecked(!this.music.isLiked());
+    }
+
+    private void bind()
+    {
+        this.musicArtistEditText = (EditText) findViewById(R.id.musicArtistEditText);
+        this.musicCategoryEditText = (EditText) findViewById(R.id.musicCategoryEditText);
+        this.musicDescriptionEditText = (EditText) findViewById(R.id.musicDescriptionEditText);
+        this.musicDontLikeRadioBtn = (RadioButton) findViewById(R.id.musicDontLikeRadioBtn);
+        this.musicLikeRadioBtn = (RadioButton) findViewById(R.id.musicLikeRadioBtn);
+        this.musicTitleEditText = (EditText) findViewById(R.id.musicTitleEditText);
 
     }
 }
