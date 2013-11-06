4eb13592678e1a874bcfc246c1c44062930a1fb1
Florian Burel <fl0@mac.com>
2013-11-06 01:10:27 +0100

    Utilisation d'un FrameLayout dans le layout pour telephone Debut d'implementation du MusicLoverActivity

--- MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
+++ MusicLovers/src/main/java/fr/florianBurel/musiquelovers/MusicLoversActivity.java
@@ -1,7 +1,9 @@
 package fr.florianBurel.musiquelovers;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 
 /**
@@ -10,6 +12,10 @@ import android.os.Bundle;
 public class MusicLoversActivity extends Activity
 {
 
+    EditFragment editFragment;
+    MusicListFragment musicListFragment;
+
+
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -17,6 +23,22 @@ public class MusicLoversActivity extends Activity
 
         setContentView(R.layout.main_layout);
 
+        // Verifie que l'on utilise un layout avec le frameLayout
+        if (findViewById(R.id.frameLayout) != null) {
 
+            // Verifie que l'on est pas restauré depuis un état précédent (rotation)
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Crée un nouveau musicListFragment
+            this.musicListFragment = new MusicListFragment();
+
+
+
+            // Add the fragment
+            getFragmentManager().beginTransaction()
+                    .add(R.id.frameLayout, this.musicListFragment).commit();
+        }
     }
 }
